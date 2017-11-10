aaf493bbaf2a5c390f6abdb1467c69c856e93edd
Max Z <max.zlotskiy@gmail.com>
2017-11-09 20:01:45 -0500

    renders the template

---
+++ rest.py
@@ -0,0 +1,20 @@
+#import useful libraries
+from flask import Flask, render_template
+import urllib2
+import json
+
+
+#make the flask object
+app = Flask()
+
+
+#bind to a route
+@app.route('/')
+def home_route():
+    return render_template("index.html")
+
+
+#start the Flask server
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
